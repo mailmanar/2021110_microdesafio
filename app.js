4068c81131a676e6afd7c4a575892fe7a25f42a3
mailmanar <marianochena@gmail.com>
2021-11-01 23:18:23 -0300

    ejercicio

--- app.js
+++ app.js
@@ -2,10 +2,12 @@ const express = require('express')
 const path = require('path')
 const app = express()
 
+const publicPath = path.resolve(__dirname,'./public')
+//console.log(publicPath)
 app.listen(3030,()=>console.log('Servidor corriendo'))
 
 
-app.use(express.static('public'));
+app.use(express.static(publicPath));
 
 
 app.get('/',function(req,res){
