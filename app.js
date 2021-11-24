f78a28334e7a1b782260f92cae10067d655057c7
mailmanar <marianochena@gmail.com>
2021-11-24 20:05:27 -0300

    Previo Entrega

--- app.js
+++ app.js
@@ -1,15 +1,19 @@
 const express = require('express')
 const path = require('path')
 const app = express()
-
-const publicPath = path.resolve(__dirname,'./public')
+const publicPath = path.resolve(__dirname,'public')
 //console.log(publicPath)
 app.listen(3030,()=>console.log('Servidor corriendo'))
 
 
 app.use(express.static(publicPath));
 
+console.log(path.join(__dirname,'views/cuenta.html'))
 
 app.get('/',function(req,res){
-    res.sendFile(path.join(__dirname,'./views/home.html'));
+    res.sendFile(path.join(__dirname,'views/home.html'));
+})
+
+app.get('/cuenta',function(req,res){
+    res.sendFile(path.join(__dirname,'views/cuenta.html'));
 })
